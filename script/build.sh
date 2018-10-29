8be785f53e01552ce5f914ce0f0ecc40d35cbd40
Leigh London <leigh.00.london@gmail.com>
2018-10-29 22:29:31 +1100

    Style update.

--- script/build.sh
+++ script/build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -eu
 for dep in "go" "git"; do
-	hash "$dep" 2>/dev/null || { echo "missing $dep :("; exit 1; }
+  hash "$dep" 2>/dev/null || { echo "missing $dep :("; exit 1; }
 done
 commit=$(git rev-parse HEAD 2>/dev/null)
 go build -ldflags "-X main.commit=$commit" -o emdee .
