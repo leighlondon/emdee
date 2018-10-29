6a8836747cd0e7d177983c1e46b40ecb9874d760
Leigh London <leigh.00.london@gmail.com>
2018-10-29 22:24:40 +1100

    Restructure.

--- main.go
+++ main.go
@@ -12,96 +12,97 @@ import (
 	"os"
 )
 
+var commit = "latest"
+
+const version = "0.6.0"
+
 const usage = "emdee " + version + `
 
     Calculate message digests for input files.
 
 Usage:
-    emdee [options] filename..
+    emdee [options] filename...
 
 Options:
-    -h    Show this screen.
-    -m    Calculate the MD5 hash.
-    -s    Calculate the SHA256 hash.
-    -s1   Calculate the SHA1 hash.
-    -v    Show the version number.
+    -h      Show this screen.
+    -md5    Calculate the MD5 hash.
+    -sha256 Calculate the SHA256 hash.
+    -sha1   Calculate the SHA1 hash.
+    -v      Show the version number.
 `
+
+type options struct {
+	md5     bool
+	sha256  bool
+	sha1    bool
+	version bool
+}
 
 func main() {
 
-	// Flags for the program, declared here for scoping.
-	md5Flag := flag.Bool("m", false, "Calculate the MD5 hash.")
-	sha256Flag := flag.Bool("s", false, "Calculate the SHA256 hash.")
-	sha1Flag := flag.Bool("s1", false, "Calculate the SHA1 hash.")
-	versionFlag := flag.Bool("v", false, "Show the version number.")
-
-	// Replace the usage screen.
-	flag.Usage = func() {
-		fmt.Printf(usage)
-	}
-
-	// Parse the flags.
+	opts := options{}
+	flag.BoolVar(&opts.md5, "md5", false, "Calculate the MD5 hash.")
+	flag.BoolVar(&opts.sha256, "sha256", false, "Calculate the SHA256 hash.")
+	flag.BoolVar(&opts.sha1, "sha1", false, "Calculate the SHA1 hash.")
+	flag.BoolVar(&opts.version, "v", false, "Show the version number.")
+	flag.Usage = func() { fmt.Printf(usage) }
 	flag.Parse()
 
 	// Check for the easy flag.
-	if *versionFlag {
-		fmt.Println(version)
+	if opts.version {
+		fmt.Printf("emdee v%s, commit %s\n", version, commit)
 		return
 	}
 
 	// If no flags were set, run everything.
 	if flag.NFlag() == 0 {
-		*md5Flag = true
-		*sha1Flag = true
-		*sha256Flag = true
+		opts.md5 = true
+		opts.sha1 = true
+		opts.sha256 = true
 	}
 
-	// Need at least one filename to be provided.
 	if flag.NArg() == 0 {
 		fmt.Println("no filename provided")
 		return
 	}
 
-	// Print the version string with the normal output.
 	fmt.Println(version)
 
-	for _, filename := range flag.Args() {
+	for _, fn := range flag.Args() {
 
-		// Load the data from file.
-		file, err := os.Open(filename)
+		f, err := os.Open(fn)
 		if err != nil {
-			fmt.Println("\n" + filename + ": unable to read file")
+			fmt.Println("\n" + fn + ": unable to read file")
 			continue
 		}
-		defer file.Close()
+		defer f.Close()
 
-		// Use a buffered reader.
-		reader := bufio.NewReader(file)
+		rdr := bufio.NewReader(f)
 
 		// Declare the hashes.
-		md5Hash := md5.New()
-		sha256Hash := sha256.New()
-		sha1Hash := sha1.New()
+		h0 := md5.New()
+		h1 := sha256.New()
+		h2 := sha1.New()
 
 		// Create a MultiWriter of all of the hashes.
-		all := io.MultiWriter(sha1Hash, sha256Hash, md5Hash)
+		all := io.MultiWriter(h0, h1, h2)
 
 		// Copy to all at once.
-		_, err = io.Copy(all, reader)
+		_, err = io.Copy(all, rdr)
 		if err != nil {
 			break
 		}
 
 		// Print the output.
-		fmt.Println("\nfile:   " + filename)
-		if *md5Flag {
-			fmt.Println("md5:    " + hex.EncodeToString(md5Hash.Sum(nil)))
+		fmt.Println("\nfile:   " + fn)
+		if opts.md5 {
+			fmt.Println("md5:    " + hex.EncodeToString(h0.Sum(nil)))
 		}
-		if *sha1Flag {
-			fmt.Println("sha1:   " + hex.EncodeToString(sha1Hash.Sum(nil)))
+		if opts.sha1 {
+			fmt.Println("sha1:   " + hex.EncodeToString(h2.Sum(nil)))
 		}
-		if *sha256Flag {
-			fmt.Println("sha256: " + hex.EncodeToString(sha256Hash.Sum(nil)))
+		if opts.sha256 {
+			fmt.Println("sha256: " + hex.EncodeToString(h1.Sum(nil)))
 		}
 	}
 }
--- version.go
+++
@@ -1,4 +0,0 @@
-package main
-
-// Version number for the program.
-const version = "0.5.0"
