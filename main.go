b23c3dc4831525c2fc7666aa88b8a7dc1855ca9b
Leigh London <leigh.00.london@gmail.com>
2018-10-29 22:25:11 +1100

    Update the API.

--- main.go
+++ main.go
@@ -7,8 +7,8 @@ import (
 	"crypto/sha256"
 	"encoding/hex"
 	"flag"
-	"fmt"
 	"io"
+	"log"
 	"os"
 )
 
@@ -26,7 +26,7 @@ Usage:
 Options:
     -h      Show this screen.
     -md5    Calculate the MD5 hash.
-    -sha256 Calculate the SHA256 hash.
+    -sha256 Calculate the SHA256 hash. (default)
     -sha1   Calculate the SHA1 hash.
     -v      Show the version number.
 `
@@ -39,70 +39,62 @@ type options struct {
 }
 
 func main() {
+	log.SetFlags(0)
 
 	opts := options{}
 	flag.BoolVar(&opts.md5, "md5", false, "Calculate the MD5 hash.")
 	flag.BoolVar(&opts.sha256, "sha256", false, "Calculate the SHA256 hash.")
 	flag.BoolVar(&opts.sha1, "sha1", false, "Calculate the SHA1 hash.")
 	flag.BoolVar(&opts.version, "v", false, "Show the version number.")
-	flag.Usage = func() { fmt.Printf(usage) }
+	flag.Usage = func() { log.Printf(usage) }
 	flag.Parse()
 
 	// Check for the easy flag.
 	if opts.version {
-		fmt.Printf("emdee v%s, commit %s\n", version, commit)
+		log.Printf("emdee v%s, commit %s\n", version, commit)
 		return
 	}
 
 	// If no flags were set, run everything.
 	if flag.NFlag() == 0 {
-		opts.md5 = true
-		opts.sha1 = true
 		opts.sha256 = true
 	}
 
 	if flag.NArg() == 0 {
-		fmt.Println("no filename provided")
+		log.Println("no filename provided")
 		return
 	}
-
-	fmt.Println(version)
 
 	for _, fn := range flag.Args() {
 
 		f, err := os.Open(fn)
 		if err != nil {
-			fmt.Println("\n" + fn + ": unable to read file")
+			log.Println("\n" + fn + ": unable to read file")
 			continue
 		}
 		defer f.Close()
 
 		rdr := bufio.NewReader(f)
 
-		// Declare the hashes.
-		h0 := md5.New()
-		h1 := sha256.New()
-		h2 := sha1.New()
+		md := md5.New()
+		s1 := sha1.New()
+		s2 := sha256.New()
 
-		// Create a MultiWriter of all of the hashes.
-		all := io.MultiWriter(h0, h1, h2)
-
-		// Copy to all at once.
+		all := io.MultiWriter(md, s1, s2)
 		_, err = io.Copy(all, rdr)
 		if err != nil {
 			break
 		}
 
-		// Print the output.
-		fmt.Println("\nfile:   " + fn)
-		if opts.md5 {
-			fmt.Println("md5:    " + hex.EncodeToString(h0.Sum(nil)))
+		var d string
+		switch {
+		case opts.md5:
+			d = hex.EncodeToString(md.Sum(nil))
+		case opts.sha1:
+			d = hex.EncodeToString(s1.Sum(nil))
+		case opts.sha256:
+			d = hex.EncodeToString(s2.Sum(nil))
 		}
-		if opts.sha1 {
-			fmt.Println("sha1:   " + hex.EncodeToString(h2.Sum(nil)))
-		}
-		if opts.sha256 {
-			fmt.Println("sha256: " + hex.EncodeToString(h1.Sum(nil)))
-		}
+		log.Printf("%s\t%s\n", fn, d)
 	}
 }
