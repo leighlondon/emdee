601ab27df1e2c24c728acd86249088c6bf016a20
Leigh London <leigh.00.london@gmail.com>
2018-10-30 13:30:06 +1100

    shellcheck y'all

--- script/build.sh
+++ script/build.sh
@@ -9,7 +9,7 @@ commit=$(git rev-parse HEAD 2>/dev/null) || die_ 'unable to get commit hash'
 oss=("darwin" "linux" "windows")
 arch="amd64" # no 386 comps in these woods folks
 
-echo "--- building for [${oss[@]}] [$arch]"
+echo "--- building for [${oss[*]}] [$arch]"
 for os in "${oss[@]}"; do
   GOOS="$os" GOARCH="$arch" CGO_ENABLED=0 \
     go build -ldflags "-X main.commit=$commit" -o "emdee-$os-$arch" .
