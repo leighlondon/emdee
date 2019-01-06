7ea6b3f19a7be5fa8241c494251124ed3a0e5574
Leigh London <leigh.00.london@gmail.com>
2019-01-06 23:02:33 +1100

    explicit help flag, move stuff around

--- main.go
+++ main.go
@@ -14,26 +14,15 @@ import (
 	"text/tabwriter"
 )
 
-var commit = "latest"
-
 const version = "0.9.0"
 
-const usage = `usage: emdee [options] filename...
-
-    Calculate message digests for input files.
-
-options:
-    -h      Show this screen.
-    -md5    Calculate the MD5 hash.
-    -sha256 Calculate the SHA256 hash. (default)
-    -sha1   Calculate the SHA1 hash.
-    -v      Show the version number.
-`
+var commit = "latest"
 
 type options struct {
 	md5     bool
 	sha256  bool
 	sha1    bool
+	help    bool
 	version bool
 }
 
@@ -41,7 +30,26 @@ type flusher interface {
 	Flush() error
 }
 
+func main() {
+	stdout := tabwriter.NewWriter(os.Stdout, 0, 0, 8, ' ', 0)
+	stderr := tabwriter.NewWriter(os.Stderr, 0, 0, 8, ' ', 0)
+
+	opts := options{}
+	flag.BoolVar(&opts.md5, "md5", false, "Calculate the MD5 hash.")
+	flag.BoolVar(&opts.sha256, "sha256", false, "Calculate the SHA256 hash.")
+	flag.BoolVar(&opts.sha1, "sha1", false, "Calculate the SHA1 hash.")
+	flag.BoolVar(&opts.help, "h", false, "Show this help screen.")
+	flag.BoolVar(&opts.help, "help", false, "Show this help screen.")
+	flag.BoolVar(&opts.version, "version", false, "Show the version number.")
+	flag.Usage = func() { fmt.Fprintf(os.Stdout, usage) }
+	flag.Parse()
+
+	os.Exit(run(stdout, stderr, &opts, flag.Args()))
+}
+
 func run(stdout io.Writer, stderr io.Writer, opts *options, names []string) int {
+	// some writers to be flushed after all calls are complete.
+	// if it has a Flush method, defer a call to it.
 	if w, ok := stdout.(flusher); ok {
 		defer w.Flush()
 	}
@@ -49,6 +57,10 @@ func run(stdout io.Writer, stderr io.Writer, opts *options, names []string) int 
 		defer w.Flush()
 	}
 
+	if opts.help {
+		fmt.Fprintf(stdout, usage)
+		return 0
+	}
 	if opts.version {
 		fmt.Fprintf(stdout, "emdee v%s, commit %s\n", version, commit)
 		return 0
@@ -99,17 +111,14 @@ func run(stdout io.Writer, stderr io.Writer, opts *options, names []string) int 
 	return 0
 }
 
-func main() {
-	stdout := tabwriter.NewWriter(os.Stdout, 0, 0, 8, ' ', 0)
-	stderr := tabwriter.NewWriter(os.Stderr, 0, 0, 8, ' ', 0)
+const usage = `usage: emdee [options] filename...
 
-	opts := options{}
-	flag.BoolVar(&opts.md5, "md5", false, "Calculate the MD5 hash.")
-	flag.BoolVar(&opts.sha256, "sha256", false, "Calculate the SHA256 hash.")
-	flag.BoolVar(&opts.sha1, "sha1", false, "Calculate the SHA1 hash.")
-	flag.BoolVar(&opts.version, "v", false, "Show the version number.")
-	flag.Usage = func() { fmt.Fprintf(os.Stdout, usage) }
-	flag.Parse()
+    Calculate message digests for input files.
 
-	os.Exit(run(stdout, stderr, &opts, flag.Args()))
-}
+options:
+    -h      Show this screen.
+    -md5    Calculate the MD5 hash.
+    -sha256 Calculate the SHA256 hash. (default)
+    -sha1   Calculate the SHA1 hash.
+    -v      Show the version number.
+`
