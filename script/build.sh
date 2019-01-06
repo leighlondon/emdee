25e37b91cbb8fac4240cf227c3091fc7a9d7b952
Leigh London <leigh.00.london@gmail.com>
2019-01-06 21:43:43 +1100

    better build script

--- script/build.sh
+++ script/build.sh
@@ -8,13 +8,15 @@ done
 commit=$(git rev-parse HEAD 2>/dev/null) || die_ 'unable to get commit hash'
 oss=("darwin" "linux" "windows")
 arch="amd64" # no 386 comps in these woods folks
+name="emdee"
 
 echo "--- building for [${oss[*]}] [$arch]"
 for os in "${oss[@]}"; do
-  name="emdee-$os-$arch"
-  echo "~~~ building $name"
+  echo "~~~ building $name-$os"
+  [[ $os == "windows" ]] && bin="$name.exe" || bin="$name"
   GOOS="$os" GOARCH="$arch" CGO_ENABLED=0 \
-    go build -ldflags "-X main.commit=$commit" -o "$name" .
+    go build -ldflags "-X main.commit=$commit" -o "$bin" .
+  zip "$name-$os.zip" "$bin" || die_ "zip problems: $name-$os"
 done
 
 echo '--- complete'
