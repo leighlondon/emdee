f94be600331c8f9bc479b8e5cdce310485159a33
Leigh London <leigh.00.london@gmail.com>
2019-09-14 09:22:03 +1000

    tidy up

--- build.sh
+++ build.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
-set -eu
-die_() { echo "[FATAL] $1"; exit 1; }
-for dep in "go" "git"; do
-  hash "$dep" 2>/dev/null || die_ "missing $dep :("
+die() { echo "[FATAL] $1"; exit 1; }
+for dep in "git" "go" "zip"; do
+  hash "$dep" 2>/dev/null || die "missing $dep :("
 done
 
-commit=$(git rev-parse HEAD 2>/dev/null) || die_ 'unable to get commit hash'
+commit=$(git rev-parse HEAD 2>/dev/null) || die 'unable to get commit hash'
 oss=("darwin" "linux" "windows")
 arch="amd64" # no 386 comps in these woods folks
 name="emdee"
@@ -15,8 +14,6 @@ for os in "${oss[@]}"; do
   echo "~~~ building $name-$os"
   [[ $os == "windows" ]] && bin="$name.exe" || bin="$name"
   GOOS="$os" GOARCH="$arch" CGO_ENABLED=0 \
-    go build -ldflags "-X main.commit=$commit" -o "$bin" .
-  zip "$name-$os.zip" "$bin" || die_ "zip problems: $name-$os"
+    go build -ldflags "-X main.commit=$commit" -o "$bin" . || die 'cant build'
+  zip "$name-$os.zip" "$bin" || die "zip problems: $name-$os"
 done
-
-echo '--- complete'
